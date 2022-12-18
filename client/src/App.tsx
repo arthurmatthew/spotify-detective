29ceac88e727ff8a0cef5d27602e2663941acb48
notjack <89240346+nootjack@users.noreply.github.com>
2022-12-17 16:50:36 -0800

    Frontend changes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,72 @@
+import { createRef, useState } from 'react'
+
+class User {
+    url: string
+    name: string
+    children: Array<User>
+    pfpUrl: string
+    constructor(
+        url: string,
+        name: string,
+        children: Array<User>,
+        pfpUrl: string
+    ) {
+        this.url = url
+        this.name = name
+        this.children = children
+        this.pfpUrl = pfpUrl
+    }
+}
+
+// TODO figure out how to find and edit nested object
+
 const App = () => {
-    return <div></div>
+    const [users, setUsers] = useState<any>(undefined) // start as undefined, become array of User s
+    const inputRef = createRef<HTMLInputElement>()
+
+    const initializeUsers = () => {
+        let url = inputRef.current?.value
+        url !== undefined && setUsers([new User(url, url, [], '')])
+    }
+
+    const getFollowers = (url: string) => {
+        fetch(
+            'http://localhost:3030/followers' +
+                new URLSearchParams({ url: url })
+        ).then((x) => console.log(x))
+    }
+
+    const getTestFollowers = () => {
+        fetch('http://localhost:3030/test').then((res) => setUsers(res.json()))
+    }
+
+    return (
+        <div>
+            <ul>
+                <li>
+                    <input ref={inputRef} type="text"></input>
+                    <button onClick={() => initializeUsers()}>+</button>
+                </li>
+                {users !== undefined && <List users={users} />}
+            </ul>
+        </div>
+    )
+}
+
+const List = ({ users }: { users: Array<User> | undefined }) => {
+    return (
+        <ul>
+            {users?.map((user) => (
+                <li key={user.name}>
+                    <div>
+                        {user.name}
+                        <button>+</button>
+                    </div>
+                    <List users={user.children} />
+                </li>
+            ))}
+        </ul>
+    )
 }
 
 export default App

7532d19fef17e0b72a3b44d9344eeb9ddad94aaa
notjack <89240346+nootjack@users.noreply.github.com>
2022-12-24 15:27:47 -0800

    Merged both follower functions into one

--- client/src/utils/followers.ts
+++ client/src/utils/followers.ts
@@ -1,83 +1,73 @@
-import React from 'react'
 import User from '../types/User'
 
-export const getFollowers = async (
-    url: string,
-    users: Array<User>,
-    setUsers: (value: React.SetStateAction<User[]>) => void
-) => {
-    const newUsers = await fetch(
-        'http://localhost:3000/followers?' + new URLSearchParams({ url: url })
-    )
-    const json = await newUsers.json()
-
-    // Update checked property
-    const updatedUsers = [...json, ...users].map((user) => {
-        if (user.url === url) {
-            return new User(
-                user.url,
-                user.name,
-                user.pfpUrl,
-                user.relevance,
-                true
-            )
-        }
-        return user
-    })
-    setUsers(updatedUsers.filter((x: User) => !(x.url == 'error')))
-}
-
-export const getFollowersMulti = async (
-    users: Array<User>,
-    setUsers: (value: React.SetStateAction<User[]>) => void
-) => {
+/**
+ * Takes an array of users and optionally a URL. If given a URL it will return a
+ * concatenation of the given array and the followers of the URL. If not given a URL,
+ * it will return a concatenation of the given array and the followers of every user
+ * in the given array.
+ * @param {Array<User>} users - Array of users
+ * @param {string} [url] - Optional URL
+ * @returns {Promise<User[]>}
+ */
+export const getFollowers = async (users: Array<User>, url?: string) => {
     let stateCopy: Array<User> = users
-
-    const newUsers = await fetch(
-        'http://localhost:3000/followers/multi?' +
-            new URLSearchParams({
-                url: JSON.stringify(
-                    [
-                        ...new Set(
-                            users.filter((x) => !x.checked).map((x) => x.url)
-                        ),
-                    ] // purpose of set is to remove duplicates
-                ),
-            })
+    let params = new URLSearchParams(
+        url != undefined
+            ? { url: url }
+            : {
+                  url: JSON.stringify([
+                      ...new Set(
+                          users.filter((x) => !x.checked).map((x) => x.url)
+                      ),
+                  ]),
+              }
     )
+    let endpoint = `http://localhost:3000/followers${
+        url != undefined ? '' : '/multi'
+    }?`
 
-    stateCopy = stateCopy.map(
-        (x) => new User(x.url, x.name, x.pfpUrl, x.relevance, true)
-    )
+    const newUsers = await fetch(endpoint + params)
+
+    url != undefined
+        ? (stateCopy = stateCopy.map((x) =>
+              x.url == url
+                  ? new User(x.url, x.name, x.pfpUrl, x.relevance, true)
+                  : x
+          ))
+        : (stateCopy = stateCopy.map(
+              (x) => new User(x.url, x.name, x.pfpUrl, x.relevance, true)
+          ))
 
     const json = await newUsers.json() // array of all followers of every user in array
 
     stateCopy = [...stateCopy, ...json].filter((x: User) => !(x.url == 'error'))
 
-    setUsers(stateCopy)
+    return stateCopy
 }
 
-export const getTestFollowers = (
-    setUsers: (value: React.SetStateAction<User[]>) => void
-) => {
-    fetch('http://localhost:3000/test').then(async (x) => {
-        let users = await x.json()
-        setUsers((prevUsers) => [
-            ...(prevUsers as any[]),
-            ...users.filter((x: User) => !(x.url == 'error')),
-        ])
-    })
+/**
+ * Fetches an array of fake users from the server.
+ * @returns {Promise<User[]>}
+ */
+export const getTestFollowers = async (): Promise<User[]> => {
+    const users = await fetch('http://localhost:3000/test')
+    const json: Array<User> = await users.json()
+    return json.filter((x: User) => !(x.url == 'error'))
 }
 
-// Essentially removes duplicates from array and counts their occurrences and adds it as the relevance property
-export const toRelevanceModel = (users: Array<User>) => {
+/**
+ * Removes users with duplicate URLs, counts the duplicates, and adds the count as a relevance key on each user.
+ *
+ * @param {Array<User>} users - An array of users
+ * @returns {User[]}
+ */
+export const toRelevanceModel = (users: Array<User>): User[] => {
     let result = users
     let counts = users
         .map((x) => x.url)
         .reduce((acc, e) => acc.set(e, (acc.get(e) || 0) + 1), new Map())
-    // counts.values() = occurrences
-    // counts.keys() = users
-    result.map((x) => (x.relevance = counts.get(x.url))) // adds relevance property
+
+    result.map((x) => (x.relevance = counts.get(x.url))) // Adds relevance property
 
     let unique = new Map()
     for (let user of result) {
@@ -89,7 +79,7 @@ export const toRelevanceModel = (users: Array<User>) => {
         }
     }
 
-    // this code essentially removes duplicates, preferring the duplicate with a checked of true
+    // Remove duplicates, prefer duplicate with a 'checked' value of true
     return [
         ...new Map(
             result
@@ -106,6 +96,6 @@ export const toRelevanceModel = (users: Array<User>) => {
                 .map((x) => [x.url, x])
         ).values(),
     ].sort(
-        (a, b) => parseInt(b.relevance) - parseInt(a.relevance) // sort by relevance greatest to least
+        (a, b) => parseInt(b.relevance) - parseInt(a.relevance) // Sort by relevance greatest to least
     )
 }

9f6f86f2318391cb898e400fdc0be0299576595c
notjack <89240346+nootjack@users.noreply.github.com>
2022-12-23 23:12:20 -0800

    Add function to get followers by array of URL

--- server/src/modules/followers.ts
+++ server/src/modules/followers.ts
@@ -48,7 +48,7 @@ const followers = async (profileUrl: string) => {
 
     console.log('Visiting site...')
     try {
-        await page.goto(url, { waitUntil: 'networkidle0' })
+        await page.goto(url, { waitUntil: 'networkidle0' }) // change this to wait for the followers element, maybe makes it faster
     } catch (err) {
         console.log('Invalid URL.')
         return [new User('error', '', '')]
@@ -83,4 +83,56 @@ const followers = async (profileUrl: string) => {
     return users
 }
 
-export default followers
+const followersMulti = async (profileUrls: Array<string>) => {
+    // urls is array of profile urls
+    let urls = profileUrls.map((x) => x + '/followers')
+    let users: Array<User> = new Array()
+
+    console.log('Starting job of ' + urls.length)
+
+    console.log('Opening browser...')
+    const browser = await puppeteer.launch({ headless: true })
+
+    console.log('Creating page...')
+    const page = await browser.newPage()
+
+    for (const url of urls) {
+        console.log('Visiting site...')
+        try {
+            await page.goto(url, { waitUntil: 'networkidle0' })
+        } catch (err) {
+            console.log('Invalid URL.')
+            return [new User('error', '', '')]
+        }
+
+        console.log('Searching for followers in DOM...')
+        let followers = await findFollowers(page)
+        if (followers !== false) {
+            console.log(`Found ${followers.length} followers`)
+
+            for (let i = 0; i < followers.length; i++) {
+                let follower = followers[i]
+                let data = await follower.$(':scope > *')
+
+                let pfpUrl: string | undefined = await findPfpUrl(data)
+                let name: string | undefined = await findName(data).then((x) =>
+                    x?.slice(9)
+                ) // Slice 9 to remove JSHandle: text
+                let url: string | undefined = await findUrl(data).then((x) =>
+                    x?.slice(9)
+                )
+                if (url !== undefined && name !== undefined)
+                    users.push(
+                        new User(url, name, pfpUrl === undefined ? '' : pfpUrl)
+                    )
+                console.log(`[${i + 1}] Follower > User completed`)
+            }
+        }
+    }
+    await browser.close()
+
+    console.log(`Completed`)
+    return users
+}
+
+export default { followers, followersMulti }

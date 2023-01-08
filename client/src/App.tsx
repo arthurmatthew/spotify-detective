97958b77392d9063b23e9d1c876d1aa9c0fd870a
notjack <89240346+nootjack@users.noreply.github.com>
2023-01-07 23:03:40 -0800

    Config + GUI Changes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,14 +3,21 @@ import User from './types/User'
 import 'reactflow/dist/style.css'
 
 import { getFollowers, getTestFollowers } from './utils/followers'
+import Config from './types/Config'
 
 const App = () => {
   const [users, setUsers] = useState<Array<User>>(
     JSON.parse(localStorage.getItem('users') || '')
   )
 
-  const inputRef = createRef<HTMLInputElement>()
+  const nameRef = createRef<HTMLInputElement>()
+  const urlRef = createRef<HTMLInputElement>()
   const timesRef = createRef<HTMLInputElement>()
+
+  const [config, setConfig] = useState<Config>({
+    // CONFIG HERE
+    maxFollowers: 6,
+  })
 
   useEffect(() => {
     localStorage.setItem('users', JSON.stringify(users))
@@ -24,7 +31,7 @@ const App = () => {
     let newUsers: Array<User> = users
     for (let i = 0; i < times; i++) {
       console.log(`Job ${i}`)
-      newUsers = await getFollowers(newUsers)
+      newUsers = await getFollowers(newUsers, config)
       console.log(`Job ${i} done`)
     }
     setUsers(newUsers)
@@ -51,36 +58,13 @@ const App = () => {
         </h1>
       </header>
       <section className="flex h-12 items-center gap-2 border-b border-t border-solid border-stone-800">
-        <form className="flex gap-2 p-2" action="#">
-          <input
-            type="text"
-            ref={inputRef}
-            placeholder="open.spotify.com/user/<id>"
-            className="bg-stone-900"
-          />
-          <button
-            className="border border-solid border-stone-400 px-2"
-            onClick={async () =>
-              setUsers(
-                await getFollowers(
-                  users,
-                  inputRef.current != null ? inputRef.current.value : ''
-                )
-              )
-            }
-            disabled={users.length > 0}
-          >
-            Get Followers
-          </button>
-        </form>
-        <div className="h-5/6 w-[1px] bg-gradient-to-t from-transparent via-stone-700 to-transparent"></div>
         <form className="flex gap-2 p-2 " action="#">
           <input
             type="number"
             min="1"
             max="10"
             ref={timesRef}
-            className="bg-stone-900"
+            className="border border-solid border-stone-400 bg-transparent"
           />
           <button
             className="border border-solid border-stone-400 px-2"
@@ -107,80 +91,133 @@ const App = () => {
         </form>
       </section>
       <main>
-        {users.length == 0 ? (
-          <h1 className="p-4 text-center text-3xl text-stone-300">
-            Fetch data to get started.
-          </h1>
-        ) : (
-          <table className="w-screen table-fixed border-separate border-spacing-2">
-            <thead>
-              <tr className="lowercase">
-                <th className="border border-solid border-stone-800 bg-stone-800 px-2 text-xl font-normal"></th>
-                <th className="border border-solid border-stone-700 px-2 text-xl font-normal">
-                  Name
-                </th>
-                <th className="border border-solid border-stone-700 px-2 text-xl font-normal">
-                  Relevance
-                </th>
-                <th className="border border-solid border-stone-700 px-2 text-xl font-normal">
-                  Follower Of
-                </th>
-                <th className="border border-solid border-stone-800 bg-stone-800 px-2 text-xl font-normal"></th>
-              </tr>
-            </thead>
-            <tbody>
-              {users.map((x) => (
-                <tr className="">
-                  <th className="flex items-center justify-center border border-solid border-stone-700 py-2">
-                    <div className="h-10 w-10 overflow-hidden rounded-full">
-                      {x.pfpUrl ? (
-                        <img
-                          src={x.pfpUrl}
-                          className="flex aspect-square h-full items-center justify-center"
-                        />
-                      ) : (
-                        <span className="flex h-full items-center justify-center text-stone-500">
-                          <i className="bi-question-octagon text-xl"></i>
-                        </span>
-                      )}
-                    </div>
-                  </th>
-                  <th className="border border-solid border-stone-700 px-2 text-xl font-normal">
-                    <a href={x.url} className="h-10 w-10">
-                      {x.name}
-                    </a>
-                  </th>
-                  <th className="border border-solid border-stone-700 px-2 text-xl font-normal">
-                    {x.relevance}
-                  </th>
-                  <th className="overflow-x-scroll border border-solid border-stone-700 px-2 text-xl font-normal">
-                    <ul className="flex gap-2">
-                      {x.parent.map((x, i, arr) => (
-                        <li>
-                          <a href={x}>
-                            {users.find((i) => i.url == x)?.name || x}
-                          </a>
-                          {i == arr.length - 1 ? '' : ','}
-                        </li>
-                      ))}
-                    </ul>
-                  </th>
-                  <th className="border border-solid border-stone-700 text-xl font-normal">
+        <table className="w-screen table-fixed border-separate border-spacing-2">
+          <thead>
+            <tr className="lowercase">
+              <th className="border border-solid border-stone-800 bg-stone-800 px-2 text-xl font-normal"></th>
+              <th className="border border-solid border-stone-700 px-2 text-xl font-normal">
+                Name
+              </th>
+              <th className="border border-solid border-stone-700 px-2 text-xl font-normal">
+                Relevance
+              </th>
+              <th className="border border-solid border-stone-700 px-2 text-xl font-normal">
+                Follower Of
+              </th>
+              <th className="border border-solid border-stone-800 bg-stone-800 px-2 text-xl font-normal"></th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr className="col-span-5">
+              <td className="border border-solid border-stone-700 px-2 text-xl font-normal"></td>
+              <td className="flex gap-2 border border-solid border-stone-700 p-2 text-xl font-normal">
+                <form className="flex flex-col gap-2">
+                  <input
+                    type="text"
+                    placeholder="Name here"
+                    className="border border-solid border-stone-700 bg-transparent p-1"
+                    ref={nameRef}
+                  />
+                  <input
+                    type="url"
+                    placeholder="URL here"
+                    className="border border-solid border-stone-700 bg-transparent p-1"
+                    ref={urlRef}
+                  />
+                </form>
+                <button
+                  className="flex flex-1 items-center justify-center border border-solid border-stone-700"
+                  onClick={() =>
+                    !users.find(
+                      (x) =>
+                        x.url == urlRef.current?.value ||
+                        x.name == nameRef.current?.value
+                    ) &&
+                    setUsers((prev) => [
+                      ...prev,
+                      new User(
+                        urlRef.current?.value || '',
+                        nameRef.current?.value || '',
+                        '',
+                        '0',
+                        [],
+                        false
+                      ),
+                    ])
+                  }
+                >
+                  <i className="bi-plus-square"></i>
+                </button>
+              </td>
+              <td className="border border-solid border-stone-700 px-2 text-xl font-normal"></td>
+              <td className="border border-solid border-stone-700 px-2 text-xl font-normal"></td>
+              <td className="border border-solid border-stone-700 px-2 text-xl font-normal"></td>
+            </tr>
+            {users.map((x) => (
+              <tr>
+                <td className="flex items-center justify-center border border-solid border-stone-700 py-2">
+                  <div className="h-10 w-10 overflow-hidden rounded-full">
+                    {x.pfpUrl ? (
+                      <img
+                        src={x.pfpUrl}
+                        className="flex aspect-square h-full items-center justify-center"
+                      />
+                    ) : (
+                      <span className="flex h-full items-center justify-center text-stone-500">
+                        <i className="bi-question-octagon text-xl"></i>
+                      </span>
+                    )}
+                  </div>
+                </td>
+                <td className="border border-solid border-stone-700 px-2 text-xl font-normal">
+                  <a href={x.url} className="h-10 w-10">
+                    {x.name}
+                  </a>
+                </td>
+                <td className="border border-solid border-stone-700 px-2 text-xl font-normal">
+                  {x.relevance}
+                </td>
+                <td className="overflow-x-scroll border border-solid border-stone-700 px-2 text-xl font-normal">
+                  <ul className="flex gap-2">
+                    {x.parent.length != 0
+                      ? x.parent.map((x, i, arr) => (
+                          <li>
+                            <a href={x}>
+                              {users.find((i) => i.url == x)?.name || x}
+                            </a>
+                            {i == arr.length - 1 ? '' : ','}
+                          </li>
+                        ))
+                      : 'Nobody'}
+                  </ul>
+                </td>
+                <td className="border border-solid border-stone-700 text-xl text-green-300">
+                  <div className="flex">
                     <button
-                      disabled={x.checked}
-                      className="rounded-sm border border-solid border-stone-700 p-2 px-6 text-green-600 disabled:text-stone-700"
                       onClick={async () =>
-                        setUsers(await getFollowers(users, x.url))
+                        setUsers(await getFollowers(users, config, x.url))
                       }
+                      className="flex-1 disabled:opacity-10"
+                      disabled={x.checked}
                     >
-                      get followers
+                      Get Followers
                     </button>
-                  </th>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        )}
+                    <button
+                      onClick={async () =>
+                        setUsers((prev) =>
+                          prev.filter((user) => user.url != x.url)
+                        )
+                      }
+                      className="flex-1 text-red-300 disabled:opacity-10"
+                    >
+                      Remove
+                    </button>
+                  </div>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </main>
     </div>
   )
@@ -190,10 +227,12 @@ const UserDisplay = ({
   user,
   users,
   setUsers,
+  config,
 }: {
   user: User
   users: Array<User>
   setUsers: (array: React.SetStateAction<User[]>) => void
+  config: any
 }) => {
   return (
     <li key={user.url} className="flex w-fit gap-2 bg-stone-100 py-2 px-3">
@@ -213,7 +252,9 @@ const UserDisplay = ({
       </ul>
       <button
         disabled={user.checked}
-        onClick={async () => setUsers(await getFollowers(users, user.url))}
+        onClick={async () =>
+          setUsers(await getFollowers(users, config, user.url))
+        }
       >
         Get Followers
       </button>
